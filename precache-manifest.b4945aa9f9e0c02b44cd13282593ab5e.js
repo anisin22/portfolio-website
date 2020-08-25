a42cf5b29b1d8d324ec5f8c4db896e26450ceb3f
anisin22 <anish.singla@mail.utoronto.ca>
2020-08-24 18:06:02 -0600

    Updates

---
+++ precache-manifest.b4945aa9f9e0c02b44cd13282593ab5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33db295428c0781e19176c5ca4c013fe",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "23f005ea3d0e8fb5efdf",
+    "url": "/portfolio-website/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "714cfae3af978338804b",
+    "url": "/portfolio-website/static/css/main.2422a1ee.chunk.css"
+  },
+  {
+    "revision": "23f005ea3d0e8fb5efdf",
+    "url": "/portfolio-website/static/js/2.06bc9163.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio-website/static/js/2.06bc9163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714cfae3af978338804b",
+    "url": "/portfolio-website/static/js/main.6081da32.chunk.js"
+  },
+  {
+    "revision": "d694d6d54b77d7720fe1",
+    "url": "/portfolio-website/static/js/runtime-main.de5f67c9.js"
+  }
+]);
