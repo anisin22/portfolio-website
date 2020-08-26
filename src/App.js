41f1aeae4b8662141edd2bfcae0f4ecf5b4a9de9
anisin22 <anish.singla@mail.utoronto.ca>
2020-08-26 11:31:12 -0600

    v2.1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import './App.css';
 import { Layout, Header, Navigation, Drawer, Content, Tooltip } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
+const mql = window.matchMedia('(max-width: 600px)');
 
 function App() {
+    
+    mql.addEventListener('change', (e) => {
+        const mobileView = e.matches;
+        if (mobileView) {
+            setNavInnerHTML(null)
+        } else {
+            
+        }
+    });
+    const setNavInnerHTML = (html) => {
+        const nav = document.getElementById('nav-bar');
+        nav.innerHTML = html;
+      };
   return (
     <div className="demo-big-content">
     <Layout fixedHeader>
         <Header className="header-color" title={<Link style={{textDecoration:'none', color: "white"}}to="/">Anish Singla</Link>}  scroll>
+            <div id='nav-bar'> 
             <Navigation>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/skills">Skills</Link>
                 <a href="https://drive.google.com/file/d/1_uFMLwiA2rryx0_wXciz8Jg_xoffLq9t/view">
-                Resume
+                    Resume
                 </a>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
+            </div>
+        
         </Header>
         <Drawer title={<Link style={{textDecoration:'none', color: "black"}}to="/">Anish Singla</Link>}>
             <Navigation>
